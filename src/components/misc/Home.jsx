d7f6fddd9289fe0216c6aaa639d65da4d85551a8
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-12-13 10:49:54 -0500

    added placeholder Home comp

--- src/components/misc/Home.jsx
+++ src/components/misc/Home.jsx
@@ -1,3 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
-  return <div>sup</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="comp-container flex justify-center">
+      <div className="home-comp flex flex-col mt-10 p-12 bg-green-300 items-center rounded-3xl">
+        <div className="text-2xl">Welcome to Backyard Buddy</div>
+        <div className="text-xl">blah blah blah blah blah</div>
+        <br></br>
+        <button
+          className="text-3xl"
+          onClick={() => {
+            navigate("/plants");
+          }}
+        >
+          Start Planning
+        </button>
+      </div>
+    </div>
+  );
 };
