322d58806601d11830b25b80280855aacd2cb4c6
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-12-30 17:50:12 -0500

    added static banner before remembering to make a new branch...

--- src/components/misc/Banner.jsx
+++ src/components/misc/Banner.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import bannerV3 from "/assets/graphics/bannerV3.svg";
 
 export const Banner = ({ token, setToken }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-40 bg-light-blue-100 flex flex-col items-center">
+    <div className="h-40 bg-light-blue-100 flex flex-col items-center z-0">
+      <img className="bg-cover z-1" src={bannerV3}></img>
       <div className="self-end">
         {token ? (
           <button
