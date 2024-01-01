2be6e61a804e88e6a4872759b07539b39f6b6a48
Luke Hardy <luke.taylor.hardy@gmail.com>
2024-01-01 17:41:57 -0500

    banner clouds are perfect except direction is wrong

--- src/components/misc/Banner.jsx
+++ src/components/misc/Banner.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import banner from "/assets/graphics/wtf.svg";
-// import clouds from "/assets/graphics/scrollingclouds.svg";
+import banner from "/assets/graphics/bannernoclouds.svg";
+import clouds from "/assets/graphics/scrollingclouds.svg";
 import birdbath from "/assets/graphics/croppedbath.gif";
 import "./Banner.css";
 
@@ -8,11 +8,22 @@ export const Banner = ({ token, setToken }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="banner-container h-[11rem] bg-light-blue-200 items-center z-0">
-      <img className="bg-cover z-2" src={banner}></img>
-      {/* <div className="z-1 clouds"></div> */}
+    <div className="banner-container h-[11rem] bg-light-blue-200 items-center">
+      <div className="infinite-scroll-wrapper">
+        <img
+          src={clouds}
+          alt="Infinite Scroll Image"
+          className="infinite-scroll-image z-1"
+        />
+        <img
+          src={clouds}
+          alt="Infinite Scroll Image"
+          className="infinite-scroll-image z-1"
+        />
+      </div>
+      <img className="bg-cover z-2 absolute top-0 left-0" src={banner}></img>
       <img
-        className="z-2 absolute left-[36.4rem] top-[1.85rem] h-[7.33rem]"
+        className="z-3 absolute left-[36.4rem] top-[1.85rem] h-[7.33rem]"
         src={birdbath}
       ></img>
 
