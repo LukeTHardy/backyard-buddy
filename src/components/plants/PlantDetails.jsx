7c7073726e857900e09fb891184a584bea73d41f
Luke Hardy <luke.taylor.hardy@gmail.com>
2024-01-03 00:05:40 -0500

    more styling tweaks for MVP

--- src/components/plants/PlantDetails.jsx
+++ src/components/plants/PlantDetails.jsx
@@ -146,7 +146,7 @@ export const PlantDetails = ({ userId }) => {
               )}
             </div>
             <div className="details-card pixel-border-blue1 flex flex-col items-center w-[40rem] px-4 mb-12 top-4 relative">
-              <div className="plant-name flex justify-between w-full m-1">
+              <div className="plant-name flex justify-between w-full">
                 <button className="text-xl text-end" onClick={previousPlant}>
                   ﹤Prev
                 </button>
