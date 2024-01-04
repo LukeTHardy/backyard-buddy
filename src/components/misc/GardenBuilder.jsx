50c1a7417986146b0198a7d6d818c37cc44abf88
Luke Hardy <luke.taylor.hardy@gmail.com>
2024-01-04 14:47:57 -0500

    branch switch commit

--- src/components/misc/GardenBuilder.jsx
+++ src/components/misc/GardenBuilder.jsx
@@ -1,11 +1,14 @@
 import "/src/components/plants/PlantsCritters.css";
+import "/src/components/plants/PixelBorder.scss";
+import testpic from "/assets/graphics/lavender.png";
 
 export const GardenBuilder = () => {
   return (
-    <div className="comp-container bg-amber-100 flex justify-center relative min-h-[80vh]">
+    <div className="comp-container flex flex-col items-center bg-amber-100 flex justify-start relative min-h-[80vh]">
       <div className="home-comp text-xl italic mt-12 px-8 mb-8 py-4 h-[4rem] pixel-border-green1">
         in development
       </div>
+      <div className="border-step3">testing</div>
     </div>
   );
 };
