027afd07bcd0b714c8aff22451ee1077ce516a8c
Luke Hardy <luke.taylor.hardy@gmail.com>
2024-01-06 22:08:14 -0500

    started over from scratch

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -1,119 +1,17 @@
-export const YourComponent = () => {
-  return (
-    <div className="comp-container">
-      <div className="auth-comp content">
-        {/* Nav pills */}
-        <ul className="nav nav-pills" role="tablist">
-          <li className="nav-item">
-            <a className="nav-link active" data-toggle="pill" href="#login">
-              Login
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" data-toggle="pill" href="#regis">
-              Register
-            </a>
-          </li>
-        </ul>
+import { useState, useEffect } from "react";
 
-        {/* Tab panes */}
-        <div className="tab-content">
-          <div id="login" className="container tab-pane active">
-            <form>
-              <div className="form-group">
-                <label htmlFor="exampleFormControlInput1">Email address</label>
-                <input
-                  type="email"
-                  className="form-control is-valid"
-                  id="exampleFormControlInput1"
-                  placeholder="name@example.com"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  Well never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input
-                  type="password"
-                  className="form-control is-invalid"
-                  id="exampleInputPassword1"
-                  placeholder="Password"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  Password incorrect.
-                </small>
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
-          </div>
-          <div id="regis" className="container tab-pane fade">
-            <form>
-              <div className="form-group">
-                <label htmlFor="InputName">Full Name</label>
-                <input
-                  type="text"
-                  className="form-control is-valid"
-                  id="InputName"
-                  placeholder="Full Name"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  Well never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="InputUsername">Username</label>
-                <input
-                  type="text"
-                  className="form-control is-valid"
-                  id="InputUsername"
-                  placeholder="Username"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  Well never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleFormControlInput2">Email address</label>
-                <input
-                  type="email"
-                  className="form-control is-valid"
-                  id="exampleFormControlInput2"
-                  placeholder="name@example.com"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  Well never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPassword2">Password</label>
-                <input
-                  type="password"
-                  className="form-control is-invalid"
-                  id="exampleInputPassword2"
-                  placeholder="Password"
-                />
-                <small id="emailHelp" className="form-text text-muted">
-                  Password incorrect.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="exampleInputPasswordVer">Verify Password</label>
-                <input
-                  type="password"
-                  className="form-control is-invalid"
-                  id="exampleInputPasswordVer"
-                  placeholder="Password"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
-          </div>
+export const Auth = () => {
+  const [tabToggle, setTabToggle] = useState("login");
+
+  return (
+    <div className="comp-container h-[100vh] bg-[#16a085] flex justify-center items-center">
+      <div className="auth-comp w-[28rem] h-[28rem] rounded-3xl border-2 border-solid border-black bg-white my-auto flex flex-col">
+        <div className="tab-btns inline h-[10rem] w-[100%] relative rounded-3xl flex">
+          <div className="tab1 w-[50%] h-[100%] rounded-3xl border-2 border-solid border-black"></div>
+          <div className="tab2 w-[50%] h-[100%] rounded-3xl border-2 border-solid border-black"></div>
         </div>
+        <div className="forms-container absolute"></div>
+        <div className="forms-container absolute"></div>
       </div>
     </div>
   );
